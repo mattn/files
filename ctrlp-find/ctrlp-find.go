84aaafba78ce205e74a98e071bd9ba7d546afcb8
mattn <mattn.jp@gmail.com>
2014-08-08 15:29:44 +0900

    Progress message

--- ctrlp-find/ctrlp-find.go
+++ ctrlp-find/ctrlp-find.go
@@ -9,6 +9,7 @@ import (
 )
 
 var ignore = flag.String("ignore", "^(.git|.hg|.svn|_darcs|.bzr)$", "Ignore directory")
+var progress = flag.Bool("progress", false, "Progress message")
 
 func main() {
 	ignorere, err := regexp.Compile(*ignore)
@@ -20,9 +21,14 @@ func main() {
 	if len(os.Args) > 1 {
 		base = os.Args[1]
 	}
+	n := 0
 	err = filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			if p, err := filepath.Abs(path); err == nil {
+				if *progress {
+					n++
+					fmt.Fprintf(os.Stderr, "\r%d            ", n)
+				}
 				fmt.Println(filepath.ToSlash(p))
 			}
 		} else {
