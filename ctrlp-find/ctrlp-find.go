73925ff2f8d2dbf6a3a31d8a9bfc17ee60a3da86
mattn <mattn.jp@gmail.com>
2014-08-08 18:57:24 +0900

    Fix handle arguments

--- ctrlp-find/ctrlp-find.go
+++ ctrlp-find/ctrlp-find.go
@@ -12,22 +12,26 @@ var ignore = flag.String("ignore", "^(.git|.hg|.svn|_darcs|.bzr)$", "Ignore dire
 var progress = flag.Bool("progress", false, "Progress message")
 
 func main() {
+	flag.Parse()
 	ignorere, err := regexp.Compile(*ignore)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	base := "."
-	if len(os.Args) > 1 {
-		base = os.Args[1]
+	if flag.NArg() > 1 {
+		base = flag.Arg(0)
 	}
 	n := 0
 	err = filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
+		if info == nil {
+			return err
+		}
 		if !info.IsDir() {
 			if p, err := filepath.Abs(path); err == nil {
 				if *progress {
 					n++
-					fmt.Fprintf(os.Stderr, "\r%d            ", n)
+					fmt.Fprintf(os.Stderr, "\r%d            \r", n)
 				}
 				fmt.Println(filepath.ToSlash(p))
 			}
